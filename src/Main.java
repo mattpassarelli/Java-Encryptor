94a6b8ee2c2565b31ec3e4c01dee81f791e225f2
mattv <mattv@10.129.29.139>
2018-06-19 09:08:21 -0400

    It encrypts. I won't add a decrypt for a while until a GUI is made tbh

--- src/Main.java
+++ src/Main.java
@@ -7,36 +7,61 @@ public class Main
 	private final static long time = System.currentTimeMillis();
 	private static int key = 0;
 	private static String str = "";
-	
+	private static char[] charArray;
+	private static int[] ASCII;
+
 	public static void main(String[] args)
 	{
+
 		System.out.println("Enter your string to encrypt: ");
 		str = scan.nextLine();
-		System.out.println("String is: " + str);
-		
-		
+
 		key = findMiddleDigitOfKey();
+			
+		charArray = str.toCharArray();
+		ASCII = new int[charArray.length];
+		encrypt();
+	}
+
+	private static void encrypt()
+	{
+		for(int i = 0; i < charArray.length; i++)
+		{
+			char character = charArray[i];
+			ASCII[i] = (int) character;
+		}
+		
+		
+		for(int i = 0; i < ASCII.length; i++)
+		{
+			ASCII[i] = ASCII[i] * key;
+		}
+		
+		System.out.println("Your new encrypted string is: ");
+		for(int i = 0; i < ASCII.length; i++)
+		{
+			System.out.print(ASCII[i] + " ");
+		}
 	}
 
 	private static int findMiddleDigitOfKey()
 	{
 		int x = (int) time;
-	    String num = Integer.toString(x);
-	    Random rand = new Random();
-	    int n = rand.nextInt(num.length());
-	    while(true)
-	    {
-	    	if (n == 0)
-	    	{
-	    		n = rand.nextInt(num.length());
-	    	}
-	    	else
-	    	{
-	    		break;
-	    	}
-	    }
-		n = Integer.parseInt("" + num.charAt(n), 10);
-		return n;
+		String num = Integer.toString(x);
+		Random rand = new Random();
+		int n = rand.nextInt(num.length());
+		while(true)
+		{
+			if(Integer.parseInt("" + num.charAt(n)) == 0)
+			{
+				n = rand.nextInt(num.length());
+			}
+			else
+			{
+				break;
+			}
+		}
+		return Integer.parseInt("" + num.charAt(n));
 	}
 
 }
